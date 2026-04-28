bd695cf25dd8ddc0013cc6aa2089efab9d72c758
David Hedding <dghedding@gmail.com>
2026-04-28 10:02:24 -0500

    set up for github pages

--- pages/js/script.js
+++
@@ -1,9 +0,0 @@
-function makeGetRequest(path, target) {
-    const xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = () => {
-        document.getElementById( target ).innerHTML = xhr.responseText;
-    }
-    xhr.open( "GET", path );
-    xhr.send();
-}
